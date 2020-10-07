081bae67e2cb8e0ed5e9258430a586c7f0e05d65
Prasanth Maddali <maddali.prasanth@gmail.com>
2020-10-08 06:56:49 +0800

    ParallelLib changes

--- CodeKata.Lib/ParallelLib.cs
+++ CodeKata.Lib/ParallelLib.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CodeKata.Core
 {
     public static class ParallelLib
     {
-        public static int SumParallely()
+        public static int SumParallely(int number1, int number2)
         {
-            return 1;
+            int sum;
+            Parallel.Invoke(new Action[] { new Action(() => { sum = number1 + number2; }) });
+            return sum;
         }
     }
 }
